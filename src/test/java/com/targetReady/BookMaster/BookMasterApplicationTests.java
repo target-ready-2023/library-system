2a581139d6d0cb20db797b8b599634a47cb93afb
anushka1403 <anushkaaman.418@gmail.com>
2023-07-14 12:51:54 +0530

    created project BookMaster

---
+++ src/test/java/com/targetReady/BookMaster/BookMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.targetReady.BookMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
