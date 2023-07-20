b937addd575ca33d023ce6a3ff15b8aa39f1f6f6
ISHAASTHANA <ish.asthana@gmail.com>
2023-07-20 16:35:27 +0530

    changes in main function

--- src/main/java/com/target/ready/library/system/LibrarySystemApplication.java
+++ src/main/java/com/target/ready/library/system/LibrarySystemApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -17,6 +19,15 @@ public class LibrarySystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
 
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:3000","http://localhost:3001");
+			}
+		};
 	}
 
 }
