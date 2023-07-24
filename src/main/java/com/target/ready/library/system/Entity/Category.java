d1045fb43df126e0dabdf54c94263fd263565b0e
ISHAASTHANA <ish.asthana@gmail.com>
2023-07-24 22:24:27 +0530

    Swagger

--- src/main/java/com/target/ready/library/system/Entity/Category.java
+++ src/main/java/com/target/ready/library/system/Entity/Category.java
@@ -1,6 +1,7 @@
 package com.target.ready.library.system.Entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Hidden
 public class Category {
     private int categoryId;
     @JsonProperty
---
+++ src/test/java/com/target/ready/library/system/Controller/categoryControllerTests.java
@@ -0,0 +1,5 @@
+package com.target.ready.library.system.Controller;
+
+
+public class categoryControllerTests {
+}
