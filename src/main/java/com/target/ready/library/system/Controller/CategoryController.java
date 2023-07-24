ef707c348a0684a7a0b8b4f4ac1d9599a8f1e2d5
ISHAASTHANA <ish.asthana@gmail.com>
2023-07-24 11:38:30 +0530

    Swagger Integrated and getAllCategories api done

--- src/main/java/com/target/ready/library/system/Controller/CategoryController.java
+++ src/main/java/com/target/ready/library/system/Controller/CategoryController.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.target.ready.library.system.Entity.Category;
 import com.target.ready.library.system.Service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("library_system/v2")
@@ -19,5 +18,10 @@ public class CategoryController {
     @PostMapping("inventory/category")
     public String addCategory(@RequestBody Category category) throws JsonProcessingException {
         return categoryService.addCategory(category);
+    }
+
+    @GetMapping("/categories")
+    public List<Category> getAllCategories(){
+        return categoryService.findAllCategories();
     }
 }
--- src/main/java/com/target/ready/library/system/LibrarySystemApplication.java
+++ src/main/java/com/target/ready/library/system/LibrarySystemApplication.java
@@ -2,11 +2,7 @@ package com.target.ready.library.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -16,15 +12,6 @@ public class LibrarySystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
 
-	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:3000","http://localhost:3001");
-			}
-		};
 	}
 
 }
