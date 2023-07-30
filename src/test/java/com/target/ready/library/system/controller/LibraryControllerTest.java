34ca87aefef095bf39944f7f4d4d50975fc70fcd
Jahnavi8122 <jahnavipabbathi812@gmail.com>
2023-07-30 22:31:52 +0530

    unit testing for getAllBooks done

--- src/test/java/com/target/ready/library/system/controller/LibraryControllerTest.java
+++ src/test/java/com/target/ready/library/system/controller/LibraryControllerTest.java
@@ -1,4 +1,50 @@
 package com.target.ready.library.system.controller;
+import com.target.ready.library.system.entity.Book;
+import com.target.ready.library.system.repository.BookImplementation;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import com.target.ready.library.system.service.LibrarySystemService;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(classes = {LibraryControllerTest.class})
 public class LibraryControllerTest {
+    @Mock
+    LibrarySystemService librarySystemService;
+
+    @InjectMocks
+    LibraryController libraryController;
+
+    @Mock
+    BookImplementation bookRepository;
+
+    @Test
+    public void testGetAllBooks() throws Exception{
+        List<Book>  records = new ArrayList<Book>();
+        records.add(new Book(1,
+                "Five Point someone",
+                "Semi-autobiographical"
+                ,"Chetan Bhagat",2004));
+        records.add(new Book(2,
+                "The Silent Patient",
+                "The dangers of unresolved or improperly treated mental illness","Alex Michaelides",2019)
+        );
+
+        when(librarySystemService.getAllBooks(0,5)).thenReturn(records);
+        ResponseEntity<List<Book>> response = libraryController.getAllBooks(0);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(2, response.getBody().size());
+
+    }
+
 }
--- src/test/java/com/target/ready/library/system/service/LibrarySystemServiceTest.java
+++ src/test/java/com/target/ready/library/system/service/LibrarySystemServiceTest.java
@@ -1,4 +1,46 @@
 package com.target.ready.library.system.service;
 
+import com.target.ready.library.system.entity.Book;
+import com.target.ready.library.system.repository.BookCategoryRepository;
+import com.target.ready.library.system.repository.BookRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.*;
+
+import java.util.*;
+
+@SpringBootTest(classes = {LibrarySystemServiceTest.class})
 public class LibrarySystemServiceTest {
+
+    @Mock
+    BookRepository bookRepository;
+
+    @Mock
+    BookCategoryRepository bookCategoryRepository;
+
+    @InjectMocks
+    LibrarySystemService librarySystemService;
+
+    @Test
+    public void findAllBooksTest(){
+        List<Book> records = new ArrayList<Book>();
+        records.add(new Book(1,
+                "Five Point someone",
+                "Semi-autobiographical"
+                ,"Chetan Bhagat",2004));
+        records.add(new Book(2,
+                "The Silent Patient",
+                "The dangers of unresolved or improperly treated mental illness","Alex Michaelides",2019)
+        );
+
+        when(bookRepository.getAllBooks(0,5)).thenReturn(records);
+        List<Book> response=librarySystemService.getAllBooks(0,5);
+        assertEquals(2, response.size());
+    }
+
 }
