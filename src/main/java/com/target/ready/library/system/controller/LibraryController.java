f8cce4a901f6fcf5716babe1db69488f58916f69
Sharanya CH <sharanyabhat29@gmail.com>
2023-08-10 10:00:04 +0530

    Exception Handling and changing some Unit testcases

--- src/main/java/com/target/ready/library/system/controller/LibraryController.java
+++ src/main/java/com/target/ready/library/system/controller/LibraryController.java
@@ -147,7 +147,11 @@ public class LibraryController {
                             mediaType = "application/json"
                     ))})
     public ResponseEntity<List<Book>> findByBookName(@PathVariable("book_name") String bookName){
-        return new ResponseEntity<>(librarySystemService.findByBookName(bookName),HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(librarySystemService.findByBookName(bookName), HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        }
 
     }
 
